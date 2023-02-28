a30c205d5938136ab890e8df5c7410ba0cf7764c
Viktor Lilja <viktorlilja2@gmail.com>
2023-02-28 11:17:20 +0100

    Al SAXS MgZn2 peaks

--- SAXS Al7075/analysis_waxs.py
+++ SAXS Al7075/analysis_waxs.py
@@ -17,31 +17,41 @@ for sample in ["130C", "160C", "190C", "220C", "250C"]:
     q = data[:,0]
     I = data[:,1]
 
-    # Calculate expected q for different peaks
-    a = 4.0479
-    # FCC structure factor: h, k, l all even or odd
-
     fig = plt.figure(figsize=(8,4))
     ax = fig.add_subplot(1,1,1)
-
     ax.set_title(f"{sample}")
-    ax.plot(q, I, label="Al7075, Aged 160C 2h")
+    ax.plot(q, I, label="Al7075, aged 160C 2h")
 
     # Aluminium planes
-    Al_a = 4.0479
-    Al_planes = [
-        [1, 0, 0],
-    ]
-    for i, plane in enumerate(Al_planes):
-        h, k, l = (1, 0, 0)
-        q_plane = 2 * np.pi * np.sqrt(h**2 + k**2 + l**2) / Al_a
-        ax.vlines(q_plane, 0, 0.001, alpha=0.3)
-        ax.annotate(f"Al ({plane[0]}{plane[1]}{plane[2]})",
-            (q_plane+0.01, 0.0001), alpha=0.5)
+    #Al_a = 4.0479
+    #Al_planes = [
+    #    [1, 0, 0],
+    #]
+    #for i, plane in enumerate(Al_planes):
+    #    h, k, l = plane
+    #    q_plane = 2 * np.pi * np.sqrt(h**2 + k**2 + l**2) / Al_a
+    #    ax.vlines(q_plane, 0, 0.001, alpha=0.3)
+    #    ax.annotate(f"Al ({plane[0]}{plane[1]}{plane[2]})",
+    #        (q_plane+0.01, 0.0001), alpha=0.5)
         
     # MgZn2 planes
-    MgZn2_a = 5.16
-    MgZn2_c = 5.16
+    MgZn2_a = 5.220
+    MgZn2_c = 8.566
+    MgZn2_planes = [
+        [0, 0, 0, 1],
+        [1, 0,-1, 1],
+        [0, 0, 0, 2],
+        [1, 0,-1, 0],
+    ]
+    for plane_no, plane in enumerate(MgZn2_planes):
+        h, k, i, l = plane
+        d_plane = 1 / np.sqrt(
+            (4/3) * (h**2 + h * k + k**2) / MgZn2_a**2 + l**2/MgZn2_c**2
+        )
+        q_plane = 2 * np.pi / d_plane
+        ax.vlines(q_plane, 0, 0.0008+0.00012*plane_no, alpha=0.3)
+        ax.annotate(f"({plane[0]}{plane[1]}{plane[2]}{plane[3]})",
+            (q_plane+0.01, 0.00073+0.00012*plane_no), alpha=0.5)
 
     ax.set_xlabel("Momentum transfer, $q$ "  + "(Å$^{-1}$)")
     ax.set_ylabel("Intensity, $I$ (A.U.)")
