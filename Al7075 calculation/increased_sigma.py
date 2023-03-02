4026218a08ce9624aa52a6399ec89111cb2765f3
Gottfrid Olsson <gottfridolsson@hotmail.com>
2023-03-02 09:49:37 +0100

    Incrase sigma calc

--- Al7075 calculation/increased_sigma.py
+++ Al7075 calculation/increased_sigma.py
@@ -15,15 +15,15 @@ def Delta_sigma_bend_dislocation_Orowan(M, G, b, L, r):
 def Delta_sigma_shear_dislocation(M, r, gamma, f, G, b):
     return (2*M/r)*np.sqrt(gamma**3*f/(G*b))
 
-def Delta_sigma_Canvas(alpha, M, G, b, N, d):
-    return alpha*M*G*b*np.sqrt(N*d)
+def Delta_sigma_Canvas(alpha, M, G, b, N, r):
+    return alpha*M*G*b*np.sqrt(N*2*r)
 
 
 # Material properties Al 7075
 a = 4.04*1e-10      #m, FCC lattice parameter
 G = 26.9e9          #Pa, shear modulus of Al
 M = 2.5             #-, Taylor number (given in lab PM)
-x = 0.9             #-, volume fraction Al (estimted from alloy composition)
+x = 0.9             #-, volume fraction Al (estimated from alloy composition)
 b = a/2             #m, Burger vector for FCC ({111} planes)
 d = 8.7e-9          #m, diameter of percipitated particles (from SAXS)
 gamma = 20e-3       #J m^-2, particle surface energy
@@ -37,7 +37,7 @@ N = L**-3                       #m^-3, number density of percipitated particles
 # Calculation and write to file
 Delta_sigma_Orowan = Delta_sigma_bend_dislocation_Orowan(M, G, b, L, r)*1e-6 #MPa
 Delta_sigma_shear = Delta_sigma_shear_dislocation(M, r, gamma, f, G, b)*1e-6 #Mpa
-Delta_sigma_new = Delta_sigma_Canvas(alpha, M, G, b, N, d)*1e-6              #MPa
+Delta_sigma_new = Delta_sigma_Canvas(alpha, M, G, b, N, r)*1e-6              #MPa
 
 with open('Al7075 calculation/TIF326_Lab_increased_sigma_Al7075.txt', 'w') as file:
     file.write(f"Delta_sigma_bend_dislocation_Orowan: {Delta_sigma_Orowan:.2e} MPa\n")
